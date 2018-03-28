5ff255ee5fbc11dd05cf8306f7ef2fa8d7a1df9f
Stanislav Vorobyev <vissi@vissi.su>
2018-03-28 12:50:25 +0300

    cs

--- interpret.php
+++ interpret.php
@@ -6,15 +6,15 @@ $redis->connect('127.0.0.1', 6379);
 
 // We need a file argument
 if (!isset($argv[1])) {
-	echo "\033[0;32mUsage: php basic.php <file>\n";
-	echo "\tWhere <file> is the BASIC file to parse\n\033[0m";
+    echo "\033[0;32mUsage: php basic.php <file>\n";
+    echo "\tWhere <file> is the BASIC file to parse\n\033[0m";
 } else {
-	// Get the file
-	$source = file_get_contents($argv[1]);
+    // Get the file
+    $source = file_get_contents($argv[1]);
 
-	// Create a new parser
-	$basic = new Basic();
-	$basic->preinterpret($source);
+    // Create a new parser
+    $basic = new Basic();
+    $basic->preinterpret($source);
     $canContinue = true;
     while($canContinue) {
         $canContinue = $basic->interpretStatement();
